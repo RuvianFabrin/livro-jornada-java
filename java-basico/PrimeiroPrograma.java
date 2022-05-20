0d0de21eed77d9a5f575effb851903598e34240f
Ruvian <ruvian.fabrin@hotmail.com>
2022-05-19 21:29:55 -0300

    Primeiro Commit

---
+++ java-basico/PrimeiroPrograma.java
@@ -0,0 +1,8 @@
+class PrimeiroPrograma {
+	public static void main(String[] args){
+		System.out.println("Alo mundo!");
+		//No terminal: javac PrimeiroPrograma.java 
+		//No terminal: java PrimeiroPrograma
+
+	}
+}
