dc19c541522104e20776d5facfd3b8e5d3b48fe9
Ruvian Souza <ruvian.fabrin@hotmail.com>
2022-05-27 23:57:18 -0300

    hashCode e Equals

--- java-basico/heranca/Pessoa.java
+++ java-basico/heranca/Pessoa.java
@@ -33,4 +33,39 @@ public abstract class Pessoa {
     //Método abstrato não tem chaves e termina com ;
     //Isso obriga as subclasses a definir esse método colocando um corpo
     public abstract void fazerAdmissao();
+
+    @Override
+    public String toString() {
+        return "Pessoa [idade=" + idade + ", nome=" + nome + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + idade;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pessoa other = (Pessoa) obj;
+        if (idade != other.idade)
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
+    
 }
